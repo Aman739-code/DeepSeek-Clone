af1d348ba2185bcea4f3aeb80656b51469a642dc
Aman Bhatnagar <amanviteee@gmail.com>
2025-07-06 21:46:48 +0530

    Fix file extension or remove invalid type

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -5,40 +5,40 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-    const wh = new Webhook(process.env.SIGNING_SECRET);
-    const headerPayload = await headers();
+  const wh = new Webhook(process.env.SIGNING_SECRET);
+  const headerPayload = await headers();
 
-    const svixHeaders = {
-        "svix-id": headerPayload.get("svix-id"),
-        "svix-signature": headerPayload.get("svix-signature"),
-    };
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id"),
+    "svix-signature": headerPayload.get("svix-signature"),
+  };
 
-    const payload = await req.json();
-    const body = JSON.stringify(payload);
-    const { data, type } = wh.verify(body, svixHeaders);
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+  const { data, type } = wh.verify(body, svixHeaders);
 
-    const userData = {
-        _id: data.id,
-        email: data.email_addresses[0].email_address,
-        name: `${data.first_name} ${data.last_name}`,
-        image: data.image_url,
-    };
+  const userData = {
+    _id: data.id,
+    email: data.email_addresses[0].email_address,
+    name: `${data.first_name} ${data.last_name}`,
+    image: data.image_url,
+  };
 
-    await connectDB();
+  await connectDB();
 
-    switch (type) {
-        case "user.created":
-            await User.create(userData);
-            break;
-        case "user.updated":
-            await User.findByIdAndUpdate(data.id, userData);
-            break;
-        case "user.deleted":
-            await User.findByIdAndDelete(data.id);
-            break;
-        default:
-            break;
-    }
+  switch (type) {
+    case "user.created":
+      await User.create(userData);
+      break;
+    case "user.updated":
+      await User.findByIdAndUpdate(data.id, userData);
+      break;
+    case "user.deleted":
+      await User.findByIdAndDelete(data.id);
+      break;
+    default:
+      break;
+  }
 
-    return NextResponse.json({ message: "Event received" });
+  return NextResponse.json({ message: "Event received" });
 }
