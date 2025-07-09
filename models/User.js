3a68c13da12b3911c5a61007a91635b1337c19c6
Aman Bhatnagar <amanviteee@gmail.com>
2025-07-09 23:39:19 +0530

    Final Commit

--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
@@ -7,9 +6,8 @@ const UserSchema = new mongoose.Schema(
         name: {type: String, required: true},
         email: {type: String, required: true},
         image: {type: String, required: false}
-    },{
-        timestamps: true
-    }
+    },
+    {timestamps: true}
 );
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema)
